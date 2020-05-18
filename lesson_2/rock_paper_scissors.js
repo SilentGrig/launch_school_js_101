7614871058a33b65e3fb1dc637092083909a47cd
Gregory Thomas <SilentGrig@gmail.com>
2020-05-18 13:16:07 +0100

    refactor rps game

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -9,6 +9,15 @@ const VALID_CHOICES = Object.keys(CHOICES);
 const PLAYER_WINS_MESSAGE = 'You won the round!';
 const COMPUTER_WINS_MESSAGE = 'Computer wins the round!';
 
+// Global Variables for Game
+let isPlaying = true;
+let playerScore = 0;
+let computerScore = 0;
+let matchWon = false;
+let roundsPlayed = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function welcomeMessage() {
   prompt(`Welcome to ${GAME_TYPE}!`);
   console.log('\n\n\n');
@@ -44,12 +53,19 @@ function prompt(message) {
 
 function getPlayAgainResponse() {
   prompt('Do you want to play again (y/n)?');
-  let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
+
+  let answer = readline.question();
+  answer = answer.trim().toLowerCase();
+
+  const validAnswers = ['y', 'yes', 'n', 'no'];
+
+  while (!validAnswers.includes(answer)) {
+    prompt("Please enter 'y' or 'n'.");
+    answer = readline.question();
+    answer = answer.trim().toLowerCase();
   }
-  return answer;
+
+  return answer[0];
 }
 
 function getPlayerChoice() {
@@ -69,12 +85,69 @@ function getComputerChoice() {
   return VALID_CHOICES[randomIndex];
 }
 
-let isPlaying = true;
-let playerScore = 0;
-let computerScore = 0;
-let matchWon = false;
-console.clear();
+function updateScores(roundWinnerMessage) {
+  if (roundWinnerMessage === PLAYER_WINS_MESSAGE) {
+    playerScore += 1;
+  } else if (roundWinnerMessage === COMPUTER_WINS_MESSAGE) {
+    computerScore += 1;
+  }
+}
 
+function pluraliseWord(word, times) {
+  if (word === 'match' && times !== 1) {
+    return word + 'es';
+  } else if (times !== 1) {
+    return word + 's';
+  }
+  return word;
+}
+
+function resetGame() {
+  matchWon = false;
+  playerScore = 0;
+  computerScore = 0;
+}
+
+function getMatchWinner() {
+  /* If match has been won returns 1 for player, 2 for computer
+   * If match hasn't been won returns 0
+  */
+  if (playerScore === 3) {
+    return 1;
+  } else if (computerScore === 3) {
+    return 2;
+  }
+  return 0;
+}
+
+function updateMatchScores(winner) {
+  if (winner === 1) {
+    playerWins += 1;
+  } else if (winner === 2) {
+    computerWins += 1;
+  }
+}
+
+function displayWinner(winner) {
+  console.log('\n');
+  if (winner === 1) {
+    prompt("You won the match!!!");
+  } else if (winner === 2) {
+    prompt("Unlucky, the computer won the match!");
+  }
+}
+
+function displayMatchScores() {
+  prompt(
+    `You've played ${roundsPlayed} ${pluraliseWord('round', roundsPlayed)}!`
+  );
+  prompt(`You have won ${playerWins} ${pluraliseWord('match', playerWins)}!`);
+  prompt(
+    `The Computer has won ${computerWins} ${pluraliseWord('match', computerWins)}!`
+  );
+}
+
+console.clear();
 welcomeMessage();
 
 // GAME LOOP
@@ -84,39 +157,36 @@ while (isPlaying) {
 
   let roundWinner = getWinner(playerChoice, computerChoice);
 
-  if (roundWinner === PLAYER_WINS_MESSAGE) {
-    playerScore += 1;
-  } else if (roundWinner === COMPUTER_WINS_MESSAGE) {
-    computerScore += 1;
-  }
+  updateScores(roundWinner);
 
   console.clear();
   prompt(`You chose ${playerChoice}, computer chose ${computerChoice}`);
   prompt(roundWinner);
   displayScores(playerScore, computerScore);
 
-  if (playerScore === 3) {
+  let matchWinner = getMatchWinner();
+
+  if (matchWinner !== 0) {
     matchWon = true;
-    console.log('\n');
-    prompt("You won the match!!!");
-  } else if (computerScore === 3) {
-    matchWon = true;
-    console.log('\n');
-    prompt("Unlucky, the computer won the match!");
+    updateMatchScores(matchWinner);
+    displayWinner(matchWinner);
   }
 
   console.log('\n');
 
   if (matchWon) {
+    roundsPlayed += 1;
     let answer = getPlayAgainResponse();
 
-    if (answer !== 'y') {
+    if (answer === 'n') {
       isPlaying = false;
-    } else { // reset game
-      matchWon = false;
-      playerScore = 0;
-      computerScore = 0;
-      console.clear();
+    } else {
+      resetGame();
     }
+    console.clear();
+    displayMatchScores();
+    console.log('\n');
   }
 }
+
+prompt('Goodbye!');
